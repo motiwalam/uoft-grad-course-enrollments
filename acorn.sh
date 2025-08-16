e729577f36649854d2921f05dcd0df4c07b99c77
Mustafa Motiwala <mustafa@motiwala.ca>
2025-08-15 20:12:15 -0700

    add percentFull

--- acorn.sh
+++ acorn.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-LtpaToken2=$(xclip -sel clip -o) python3 acorn.py $@ | jq '.responseObject | {course: .displayName, title: .title + ": " + .meetings.[0].subTitle} + (.meetings.[] | {totalSpace, enrollSpace, enrollmentSpaceAvailable})'
+LtpaToken2=$(xclip -sel clip -o) python3 acorn.py $@ | jq "$(cat <<JQPROG
+    .responseObject
+    | {course: .displayName, 
+       title: .title + ": " + .meetings.[0].subTitle}
+      + (.meetings.[] | {totalSpace, enrollSpace, enrollmentSpaceAvailable})
+      + (.meetings.[] | {percentFull: (.enrollSpace - .enrollmentSpaceAvailable)/.enrollSpace * 100})
+JQPROG
+)"
 
